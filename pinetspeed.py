1bc4ff77a02b3b4500aef3cdeeedcf583488324b
Rob M <robmarkoski@users.noreply.github.com>
2017-07-21 11:54:37 +0800

    0.2.2 Bugfixes
    Increased the timeout to fix some errors with slower connections.
    
    Added some error checking when calling the speedtest command.
    
    Also given chrons limited Path scope added full path for speedtest-cli. May need to fix this going forward.

--- pinetspeed.py
+++ pinetspeed.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python
-################################################################
+##############################################################################
 ## AUTHOR: Robert Markoski - 2017
-## VERSION: 0.2.1
+## VERSION: 0.2.2
 ## LICENSE: GPL 3 [https://www.gnu.org/licenses/gpl.html]
 ## VERSION-INFO:
+##      0.2.2   Added some error checking. Increased timeout on command for
+##              slower connections.
 ##      0.2.1   Fixed error when running as chron task.
 ##      0.2     Added ability to select Bytes or Bits, and now only asking for
 ##              simplified output.
 ##      0.1     Initial Release. i.e. It Works!
-################################################################
+##############################################################################
 
 import subprocess
 from ISStreamer.Streamer import Streamer
@@ -23,9 +25,13 @@ BYTES = 0 # Set to 1 if want to use MBytes/s rather than Mbits/s
 
 def get_speeds():
     """Function runs speedtest and splits out the results"""
-    speedtest_args = "--simple --bytes" if BYTES == 1 else "--simple" #Determine arguments.
-    speedtest_command = "speedtest-cli " + speedtest_args #Build Command
-    speedtest = subprocess.check_output(speedtest_command, shell=True).split() #Run Test
+    speedtest_args = "--simple --timeout 20 --bytes" if BYTES == 1 else "--simple --timeout 20" #Determine arguments.
+    speedtest_command = "python /usr/local/bin/speedtest-cli " + speedtest_args #Build Command
+    try:
+        speedtest = subprocess.check_output(speedtest_command, shell=True).split() #Run Test
+    except subprocess.CalledProcessError as e:
+        print "subprocess CalledError.output: " + e.output
+
     results = list(speedtest[i] for i in [1, 4, 7]) # Get Ping, Download, Upload
     return results
 
